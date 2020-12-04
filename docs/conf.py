ba1bb9926d3f922c1cd3628289333972f06157b0
giles <gilesknap@gmail.com>
2020-12-04 09:20:37 +0000

    only build 1 set of docs per CI

--- docs/conf.py
+++ docs/conf.py
@@ -143,7 +143,7 @@ html_favicon = "PandA-logo.ico"
 
 # sphinx-multiversion config
 smv_tag_whitelist = r"^\d+\.\d+.*$"  # only document tags with form 0.9*
-smv_branch_whitelist = r"^master$"  # only branch to document is master
+smv_branch_whitelist = r"^master|sphinx-build$"  # only branch to document is master
 smv_outputdir_format = "{ref.name}"
 smv_prefer_remote_refs = True
 smv_remote_whitelist = "origin|github"
