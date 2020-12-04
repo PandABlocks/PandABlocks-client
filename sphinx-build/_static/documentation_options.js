85843c0052544b6f14e567b0ef2bad27dbec44d1
gilesknap <gilesknap@users.noreply.github.com>
2020-12-04 10:10:49 +0000

    Deploying to gh-pages from  @ a67a13e21731199f291daac50edb94b32eebdfa5 🚀

--- sphinx-build/_static/documentation_options.js
+++ sphinx-build/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0+untagged.gba1bb99',
+    VERSION: '0.0+untagged.ga67a13e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
