0ef17180ba3667508d160579852310afe489866b
AlexWells <alex.wells@diamond.ac.uk>
2021-09-16 11:12:59 +0100

    Add tests for creating records of most type-subtype pairs
    A few special cases that need separate tests have not been completed yet.
    A few other tests in here as well, for some standalone functions.

--- tests/test_ioc.py
+++ tests/test_ioc.py
@@ -1,8 +1,25 @@
 import pytest
 
 from pandablocks.asyncio import AsyncioClient
-from pandablocks.ioc import IocRecordFactory
-from pandablocks.responses import BitOutFieldInfo, PosOutFieldInfo, TimeFieldInfo
+from pandablocks.ioc import (
+    IocRecordFactory,
+    _ensure_block_number_present,
+    _epics_to_panda_name,
+    _panda_to_epics_name,
+)
+from pandablocks.responses import (
+    BitMuxFieldInfo,
+    BitOutFieldInfo,
+    EnumFieldInfo,
+    ExtOutFieldInfo,
+    FieldInfo,
+    PosMuxFieldInfo,
+    PosOutFieldInfo,
+    ScalarFieldInfo,
+    SubtypeTimeFieldInfo,
+    TimeFieldInfo,
+    UintFieldInfo,
+)
 
 TEST_PREFIX = "TEST-PREFIX"
 counter = 0
@@ -10,17 +27,43 @@ counter = 0
 
 @pytest.fixture
 def ioc_record_factory():
+    """Create a new IocRecordFactory instance with a new, unique, namespace.
+    This means each test can run in the same process, as each test will get
+    its own namespace.
+    """
     global counter
     counter += 1
-    # Ensure we pass a different test prefix each time, so each test uses its
-    # own device name prefix
     return IocRecordFactory(TEST_PREFIX + str(counter), AsyncioClient("123"))
 
 
 TEST_RECORD = "TEST:RECORD"
 
 
-# TODO: A test for every known type-subtype pairing
+def test_panda_to_epics_name_conversion():
+    assert _panda_to_epics_name("ABC.123.456") == "ABC:123:456"
+
+
+def test_epics_to_panda_name_conversion():
+    assert _epics_to_panda_name("ABC:123:456") == "ABC.123.456"
+
+
+def test_panda_to_epics_and_back_name_conversion():
+    """Test panda->EPICS->panda round trip name conversion works"""
+    assert _epics_to_panda_name(_panda_to_epics_name("ABC.123.456")) == "ABC.123.456"
+
+
+def test_ensure_block_number_present():
+    assert _ensure_block_number_present("ABC.DEF.GHI") == "ABC1.DEF.GHI"
+    assert _ensure_block_number_present("JKL1.MNOP") == "JKL1.MNOP"
+
+
+# TODO: Test the special types
+# Tests for every known type-subtype pair except the following, which have their own
+# separate tests:
+# ext_out - bits
+# table
+# param - action
+# read - action
 @pytest.mark.parametrize(
     "field_info, values, expected_records",
     [
@@ -35,6 +78,41 @@ TEST_RECORD = "TEST:RECORD"
                 f"{TEST_RECORD}:UNITS": "s",
             },
             [f"{TEST_RECORD}", f"{TEST_RECORD}:UNITS", f"{TEST_RECORD}:MIN"],
+        ),
+        (
+            SubtypeTimeFieldInfo(
+                "param",
+                "time",
+                units_labels=["s", "ms", "min"],
+            ),
+            {
+                f"{TEST_RECORD}": "1",
+                f"{TEST_RECORD}:UNITS": "s",
+            },
+            [f"{TEST_RECORD}", f"{TEST_RECORD}:UNITS"],
+        ),
+        (
+            SubtypeTimeFieldInfo(
+                "read",
+                "time",
+                units_labels=["s", "ms", "min"],
+            ),
+            {
+                f"{TEST_RECORD}": "1",
+                f"{TEST_RECORD}:UNITS": "s",
+            },
+            [f"{TEST_RECORD}", f"{TEST_RECORD}:UNITS"],
+        ),
+        (
+            SubtypeTimeFieldInfo(
+                "write",
+                "time",
+                units_labels=["s", "ms", "min"],
+            ),
+            {
+                f"{TEST_RECORD}:UNITS": "s",
+            },
+            [f"{TEST_RECORD}", f"{TEST_RECORD}:UNITS"],
         ),
         (
             BitOutFieldInfo(
@@ -64,11 +142,275 @@ TEST_RECORD = "TEST:RECORD"
                 f"{TEST_RECORD}:UNITS",
             ],
         ),
+        (
+            ExtOutFieldInfo("ext_out", "timestamp", capture_labels=["No", "Diff"]),
+            {
+                f"{TEST_RECORD}:CAPTURE": "Diff",
+            },
+            [
+                f"{TEST_RECORD}",
+                f"{TEST_RECORD}:CAPTURE",
+            ],
+        ),
+        (
+            ExtOutFieldInfo("ext_out", "samples", capture_labels=["No", "Diff"]),
+            {
+                f"{TEST_RECORD}:CAPTURE": "Diff",
+            },
+            [
+                f"{TEST_RECORD}",
+                f"{TEST_RECORD}:CAPTURE",
+            ],
+        ),
+        (
+            BitMuxFieldInfo(
+                "bit_mux",
+                max_delay=5,
+                labels=["TTLIN1.VAL", "TTLIN2.VAL", "TTLIN3.VAL"],
+            ),
+            {
+                f"{TEST_RECORD}": "SRGATE4.OUT",
+                f"{TEST_RECORD}:DELAY": "0",
+                f"{TEST_RECORD}:MAX_DELAY": "31",
+            },
+            [
+                f"{TEST_RECORD}",
+                f"{TEST_RECORD}:DELAY",
+                f"{TEST_RECORD}:MAX_DELAY",
+            ],
+        ),
+        (
+            PosMuxFieldInfo(
+                "pos_mux",
+                labels=["INENC1.VAL", "INENC2.VAL", "INENC3.VAL"],
+            ),
+            {
+                f"{TEST_RECORD}": "ZERO",
+            },
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            UintFieldInfo(
+                "param",
+                "uint",
+                max=63,
+            ),
+            {
+                f"{TEST_RECORD}": "0",
+            },
+            [
+                f"{TEST_RECORD}",
+                f"{TEST_RECORD}:MAX",
+            ],
+        ),
+        (
+            UintFieldInfo(
+                "read",
+                "uint",
+                max=63,
+            ),
+            {
+                f"{TEST_RECORD}": "0",
+            },
+            [
+                f"{TEST_RECORD}",
+                f"{TEST_RECORD}:MAX",
+            ],
+        ),
+        (
+            UintFieldInfo(
+                "write",
+                "uint",
+                max=63,
+            ),
+            {},
+            [
+                f"{TEST_RECORD}",
+                f"{TEST_RECORD}:MAX",
+            ],
+        ),
+        (
+            FieldInfo(
+                "param",
+                "int",
+            ),
+            {
+                f"{TEST_RECORD}": "0",
+            },
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            FieldInfo(
+                "read",
+                "int",
+            ),
+            {
+                f"{TEST_RECORD}": "0",
+            },
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            FieldInfo(
+                "write",
+                "int",
+            ),
+            {},
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            ScalarFieldInfo("param", "scalar", offset=0, scale=0.001, units="deg"),
+            {
+                f"{TEST_RECORD}": "48.48",
+            },
+            [
+                f"{TEST_RECORD}",
+                f"{TEST_RECORD}:OFFSET",
+                f"{TEST_RECORD}:SCALE",
+                f"{TEST_RECORD}:UNITS",
+            ],
+        ),
+        (
+            ScalarFieldInfo("read", "scalar", offset=0, scale=0.001, units="deg"),
+            {
+                f"{TEST_RECORD}": "48.48",
+            },
+            [
+                f"{TEST_RECORD}",
+                f"{TEST_RECORD}:OFFSET",
+                f"{TEST_RECORD}:SCALE",
+                f"{TEST_RECORD}:UNITS",
+            ],
+        ),
+        (
+            ScalarFieldInfo("write", "scalar", offset=0, scale=0.001, units="deg"),
+            {},
+            [
+                f"{TEST_RECORD}",
+                f"{TEST_RECORD}:OFFSET",
+                f"{TEST_RECORD}:SCALE",
+                f"{TEST_RECORD}:UNITS",
+            ],
+        ),
+        (
+            FieldInfo(
+                "param",
+                "bit",
+            ),
+            {
+                f"{TEST_RECORD}": "0",
+            },
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            FieldInfo(
+                "read",
+                "bit",
+            ),
+            {
+                f"{TEST_RECORD}": "0",
+            },
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            FieldInfo(
+                "write",
+                "bit",
+            ),
+            {},
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            FieldInfo(
+                "write",
+                "action",
+            ),
+            {
+                f"{TEST_RECORD}": "0",
+            },
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            FieldInfo(
+                "param",
+                "lut",
+            ),
+            {
+                f"{TEST_RECORD}": "0x00000000",
+            },
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            FieldInfo(
+                "read",
+                "lut",
+            ),
+            {
+                f"{TEST_RECORD}": "0x00000000",
+            },
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            FieldInfo(
+                "write",
+                "lut",
+            ),
+            {},
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            EnumFieldInfo("param", "enum", labels=["Value", "-Value"]),
+            {
+                f"{TEST_RECORD}": "-Value",
+            },
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            EnumFieldInfo("read", "enum", labels=["Value", "-Value"]),
+            {
+                f"{TEST_RECORD}": "-Value",
+            },
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
+        (
+            EnumFieldInfo("write", "enum", labels=["Value", "-Value"]),
+            {},
+            [
+                f"{TEST_RECORD}",
+            ],
+        ),
     ],
 )
 def test_create_record(
     ioc_record_factory: IocRecordFactory, field_info, values, expected_records
 ):
+    """Test that the expected records are returned for each field info and values
+    inputs"""
     returned_records = ioc_record_factory.create_record(TEST_RECORD, field_info, values)
     assert len(returned_records) == len(expected_records)
     assert all(key in returned_records for key in expected_records)
