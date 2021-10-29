30597ef0eaba52f36835502ea38174fdc2df3703
AlexWells <alex.wells@diamond.ac.uk>
2021-10-29 14:52:50 +0100

    Small test for extra method in _types

--- tests/ioc/test_types.py
+++ tests/ioc/test_types.py
@@ -1,21 +1,30 @@
 from pandablocks.ioc._types import (
     EpicsName,
     PandAName,
-    _epics_to_panda_name,
-    _panda_to_epics_name,
+    epics_to_panda_name,
+    panda_to_epics_name,
+    trim_description,
 )
 
 
 def test_panda_to_epics_name_conversion() -> None:
-    assert _panda_to_epics_name(PandAName("ABC.123.456")) == EpicsName("ABC:123:456")
+    assert panda_to_epics_name(PandAName("ABC.123.456")) == EpicsName("ABC:123:456")
 
 
 def test_epics_to_panda_name_conversion() -> None:
-    assert _epics_to_panda_name(EpicsName("ABC:123:456")) == PandAName("ABC.123.456")
+    assert epics_to_panda_name(EpicsName("ABC:123:456")) == PandAName("ABC.123.456")
 
 
 def test_panda_to_epics_and_back_name_conversion() -> None:
     """Test panda->EPICS->panda round trip name conversion works"""
-    assert _epics_to_panda_name(
-        _panda_to_epics_name(PandAName("ABC.123.456"))
+    assert epics_to_panda_name(
+        panda_to_epics_name(PandAName("ABC.123.456"))
     ) == PandAName("ABC.123.456")
+
+
+def test_trim_description():
+    """Test trim_description for a few cases"""
+    assert trim_description("ABC", "SomeRecordName") == "ABC"
+    long_desc = "a very long string too long to fit in fact"
+    assert trim_description(long_desc, "SomeRecordName") == long_desc[0:40]
+    assert trim_description(None, "SomeRecordName") is None
