7339b85ee695fe7ae84aeeeabe29d79e0e26c480
Eva Lott <evalott100@gmail.com>
2023-10-18 09:30:32 +0100

    restored old __main__

--- src/pandablocks/__main__.py
+++ src/pandablocks/__main__.py
@@ -1,16 +1,6 @@
-from argparse import ArgumentParser
+from pandablocks.cli import cli
 
-from . import __version__
-
-__all__ = ["main"]
-
-
-def main(args=None):
-    parser = ArgumentParser()
-    parser.add_argument("-v", "--version", action="version", version=__version__)
-    args = parser.parse_args(args)
-
-
-# test with: python -m pandablocks
+# test with:
+#     pipenv run python -m pandablocks
 if __name__ == "__main__":
-    main()
+    cli()
