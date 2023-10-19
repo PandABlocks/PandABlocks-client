7e74ba699dd7acb46d50c47ce06310087c6f2517
Eva Lott <evalott100@gmail.com>
2023-10-19 10:27:55 +0100

    Changed logo back to panda

--- docs/conf.py
+++ docs/conf.py
@@ -201,5 +201,5 @@ html_show_sphinx = False
 html_show_copyright = True
 
 # Logo
-html_logo = "images/dls-logo.svg"
-html_favicon = "images/dls-favicon.ico"
+html_logo = "images/PandA-logo-for-black-background.svg"
+html_favicon = "images/PandA-logo.ico"
